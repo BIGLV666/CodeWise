6a10ce9c1f05be5c99c44ce5efe26cf1cb6d0173
LV <379299583@qq.com>
2026-07-05 19:26:56 +0800

    feat: 修正修改密码逻辑，新增验证码发送html

---
+++ service-review/src/test/java/org/example/servicereview/ServiceReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.servicereview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
